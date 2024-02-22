1318ad5ecef854a554665433642a96d6af0feece
Andrii <143963266+andriib77@users.noreply.github.com>
2024-02-22 19:55:55 +0100

    lesson3_useState update

--- src/App.jsx
+++ src/App.jsx
@@ -1,18 +1,19 @@
-import { useState } from "react";
+import { useEffect } from "react";
+
 import reactLogo from "./assets/react.svg";
 import viteLogo from "/vite.svg";
 import "./App.css";
 
 function App() {
-  const [count, setCount] = useState(0);
+  useEffect(() => {}, []);
 
   return (
     <>
-      <h1>Lesson 2</h1>
-      <img src="" alt="" />
-      <img src="" alt="" />
-      <img src="" alt="" />
-      <img src="" alt="" />
+      <h1>Lesson 3</h1>
+      <div></div>
+      <div></div>
+      <div></div>
+      <div></div>
     </>
   );
 }

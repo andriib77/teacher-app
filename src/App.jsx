30312747fba25ba67f64f19303de1d44b2c1cf27
Andrii <143963266+andriib77@users.noreply.github.com>
2024-02-22 18:54:09 +0100

    create lesson 1

---
+++ src/App.jsx
@@ -0,0 +1,23 @@
+import { useState } from "react";
+import reactLogo from "./assets/react.svg";
+import viteLogo from "/vite.svg";
+import "./App.css";
+
+function App() {
+  const [count, setCount] = useState(0);
+
+  return (
+    <>
+      <h1>Lesson 1</h1>
+      <ol>
+        <li></li>
+        <li></li>
+        <li></li>
+        <li></li>
+        <li></li>
+      </ol>
+    </>
+  );
+}
+
+export default App;
